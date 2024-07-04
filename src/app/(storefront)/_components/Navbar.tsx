1623e7f6193c7dddccd647037a49c809c5f32722
pelumini <pelumini@gmail.com>
2024-07-04 13:41:36 +0100

    stripe payment functionalities added

--- src/app/(storefront)/_components/Navbar.tsx
+++ src/app/(storefront)/_components/Navbar.tsx
@@ -15,8 +15,7 @@ export const Navbar = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  //const cart: Cart | null = await redis.get(`cart-${user?.id}`);
-  const cart: Cart | null = { userId: "", items: [] };
+  const cart: Cart | null = await redis.get(`cart-${user?.id}`);
 
   const total = cart?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
 
--- src/app/(storefront)/product/[id]/page.tsx
+++ src/app/(storefront)/product/[id]/page.tsx
@@ -33,6 +33,7 @@ const ProductIdRoute = async ({ params }: { params: { id: string } }) => {
   noStore();
   const data = await getData(params.id);
   const addProducttoShoppingCart = addItem.bind(null, data.id);
+
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start lg:gap-x-24 py-6">
